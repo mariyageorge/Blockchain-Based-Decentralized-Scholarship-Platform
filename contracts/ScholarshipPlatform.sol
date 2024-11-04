d2507e66172d482f042fcb0067861bc660ae6073
Mariya George <gmariya731@gmail.com>
2024-11-04 11:44:42 +0530

    ScholarshipPlatform.sol

--- contracts/ScholarshipPlatform.sol
+++ contracts/ScholarshipPlatform.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.26;
 contract ScholarshipPlatform {
     struct Applicant {
         string name;
-        string personalEmail; 
+        string personalEmail;
         bool verified;
         bool exists;
         uint256 fundsReceived;
+        string proofIpfsHash; 
+        string username;
+        string password; 
     }
 
     struct Sponsor {
@@ -17,56 +20,49 @@ contract ScholarshipPlatform {
         bool exists;
     }
 
-    mapping(address => Applicant) public applicants;
-    mapping(address => Sponsor) public sponsors;
-    mapping(string => bool) private verifiedEmails;
-    mapping(string => bool) private uniqueIds;
-    mapping(string => address) private uniqueIdToAddress; // Map unique IDs to sponsor addresses
+    mapping(address => Applicant) public applicants; 
+    mapping(address => Sponsor) public sponsors; 
+    mapping(string => address) public uniqueIdToAddress; 
 
-    event ApplicantRegistered(address applicant, string name, string personalEmail);
+    Applicant[] public applicantList; 
+    Sponsor[] public sponsorList; 
+
+    event ApplicantRegistered(address applicant, string name, string personalEmail, string proofIpfsHash);
     event SponsorRegistered(address sponsor, string name, string email, string uniqueId);
     event DonationMade(address indexed sponsor, address indexed applicant, uint256 amount);
     event HelpRequested(address indexed applicant, string message);
 
-    modifier onlyVerifiedApplicant() {
-        require(applicants[msg.sender].verified, "Not a verified applicant");
-        _;
-    }
-
-
-    function generateUniqueId(string memory _name, string memory _email) private pure returns (string memory) {
-        return string(abi.encodePacked(keccak256(abi.encodePacked(_name, _email))));
-    }
-
-    function registerApplicant(string memory _name, string memory _personalEmail) public {
+    
+    function registerApplicant(
+        string memory _name,
+        string memory _personalEmail,
+        string memory _proofIpfsHash,
+        string memory _username,
+        string memory _password
+    ) public {
         require(!applicants[msg.sender].exists, "Applicant already registered");
-        require(!verifiedEmails[_personalEmail], "Email already in use");
 
         applicants[msg.sender] = Applicant({
             name: _name,
             personalEmail: _personalEmail,
             verified: false,
             exists: true,
-            fundsReceived: 0
+            fundsReceived: 0,
+            proofIpfsHash: _proofIpfsHash,
+            username: _username,
+            password: _password 
         });
 
-        verifiedEmails[_personalEmail] = true;
+        applicantList.push(applicants[msg.sender]);
 
-        emit ApplicantRegistered(msg.sender, _name, _personalEmail);
+        emit ApplicantRegistered(msg.sender, _name, _personalEmail, _proofIpfsHash);
     }
 
-    function verifyApplicant(address _applicant) public {
-        require(applicants[_applicant].exists, "Applicant does not exist");
-        applicants[_applicant].verified = true;
-    }
-
+   
     function registerSponsor(string memory _name, string memory _email) public {
         require(!sponsors[msg.sender].exists, "Sponsor already registered");
 
-
         string memory _uniqueId = generateUniqueId(_name, _email);
-        require(!uniqueIds[_uniqueId], "Unique ID already in use");
-
         sponsors[msg.sender] = Sponsor({
             name: _name,
             email: _email,
@@ -74,15 +70,31 @@ contract ScholarshipPlatform {
             exists: true
         });
 
-        uniqueIds[_uniqueId] = true;
-        uniqueIdToAddress[_uniqueId] = msg.sender;
-
+        sponsorList.push(sponsors[msg.sender]); 
         emit SponsorRegistered(msg.sender, _name, _email, _uniqueId);
     }
 
-
+    
     function sponsorLogin(string memory _uniqueId) public view returns (bool) {
         return sponsors[uniqueIdToAddress[_uniqueId]].exists;
+    }
+
+ 
+    function applicantLogin(string memory _username, string memory _password) public view returns (bool) {
+        Applicant storage applicant = applicants[msg.sender];
+        require(applicant.exists, "Applicant does not exist");
+        return (keccak256(abi.encodePacked(applicant.username)) == keccak256(abi.encodePacked(_username)) &&
+                keccak256(abi.encodePacked(applicant.password)) == keccak256(abi.encodePacked(_password)));
+    }
+
+ 
+    function getAllApplicants() public view returns (Applicant[] memory) {
+        return applicantList;
+    }
+
+   
+    function getAllSponsors() public view returns (Sponsor[] memory) {
+        return sponsorList;
     }
 
     
@@ -96,18 +108,19 @@ contract ScholarshipPlatform {
         emit DonationMade(msg.sender, _applicant, msg.value);
     }
 
-
-    function requestHelp(string memory _message) public onlyVerifiedApplicant {
-        emit HelpRequested(msg.sender, _message);
+   
+    function generateUniqueId(string memory _name, string memory _email) private pure returns (string memory) {
+        return string(abi.encodePacked(keccak256(abi.encodePacked(_name, _email))));
     }
 
-    function getApplicantDetails(address _applicant) public view returns (string memory, string memory, bool, uint256) {
+  
+    function getApplicantDetails(address _applicant) public view returns (string memory, string memory, bool, uint256, string memory) {
         Applicant memory applicant = applicants[_applicant];
-        return (applicant.name, applicant.personalEmail, applicant.verified, applicant.fundsReceived);
+        return (applicant.name, applicant.personalEmail, applicant.verified, applicant.fundsReceived, applicant.proofIpfsHash);
     }
 
+   
     function getSponsorDetails(address _sponsor) public view returns (string memory, string memory, bool) {
-
         Sponsor memory sponsor = sponsors[_sponsor];
         return (sponsor.name, sponsor.uniqueId, sponsor.exists);
     }
