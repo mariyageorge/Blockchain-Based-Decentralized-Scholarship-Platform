720435cd0d199b898e952b66256978da94b33a97
Kalyanysnair <kalyanysnair2027@mca.ajce.in>
2024-11-07 22:10:32 +0530

    ScholarshipPlatform.sol

--- contracts/ScholarshipPlatform.sol
+++ contracts/ScholarshipPlatform.sol
@@ -5,56 +5,46 @@ contract ScholarshipPlatform {
     struct Applicant {
         string name;
         string personalEmail;
-        bool verified;
         bool exists;
         uint256 fundsReceived;
         string proofIpfsHash; 
-        string username;
-        string password; 
     }
 
     struct Sponsor {
         string name;
         string email;
-        string uniqueId;
         bool exists;
     }
 
     mapping(address => Applicant) public applicants; 
     mapping(address => Sponsor) public sponsors; 
-    mapping(string => address) public uniqueIdToAddress; 
 
     Applicant[] public applicantList; 
     Sponsor[] public sponsorList; 
 
     event ApplicantRegistered(address applicant, string name, string personalEmail, string proofIpfsHash);
-    event SponsorRegistered(address sponsor, string name, string email, string uniqueId);
+    event SponsorRegistered(address sponsor, string name, string email);
     event DonationMade(address indexed sponsor, address indexed applicant, uint256 amount);
-    event HelpRequested(address indexed applicant, string message);
+    event Withdrawal(address indexed applicant, uint256 amount);
+    event ProfileUpdated(address indexed user, string name, string email);
 
     
     function registerApplicant(
         string memory _name,
         string memory _personalEmail,
-        string memory _proofIpfsHash,
-        string memory _username,
-        string memory _password
+        string memory _proofIpfsHash
     ) public {
         require(!applicants[msg.sender].exists, "Applicant already registered");
 
         applicants[msg.sender] = Applicant({
             name: _name,
             personalEmail: _personalEmail,
-            verified: false,
             exists: true,
             fundsReceived: 0,
-            proofIpfsHash: _proofIpfsHash,
-            username: _username,
-            password: _password 
+            proofIpfsHash: _proofIpfsHash
         });
 
         applicantList.push(applicants[msg.sender]);
-
         emit ApplicantRegistered(msg.sender, _name, _personalEmail, _proofIpfsHash);
     }
 
@@ -62,45 +52,20 @@ contract ScholarshipPlatform {
     function registerSponsor(string memory _name, string memory _email) public {
         require(!sponsors[msg.sender].exists, "Sponsor already registered");
 
-        string memory _uniqueId = generateUniqueId(_name, _email);
         sponsors[msg.sender] = Sponsor({
             name: _name,
             email: _email,
-            uniqueId: _uniqueId,
             exists: true
         });
 
         sponsorList.push(sponsors[msg.sender]); 
-        emit SponsorRegistered(msg.sender, _name, _email, _uniqueId);
-    }
-
-    
-    function sponsorLogin(string memory _uniqueId) public view returns (bool) {
-        return sponsors[uniqueIdToAddress[_uniqueId]].exists;
-    }
-
- 
-    function applicantLogin(string memory _username, string memory _password) public view returns (bool) {
-        Applicant storage applicant = applicants[msg.sender];
-        require(applicant.exists, "Applicant does not exist");
-        return (keccak256(abi.encodePacked(applicant.username)) == keccak256(abi.encodePacked(_username)) &&
-                keccak256(abi.encodePacked(applicant.password)) == keccak256(abi.encodePacked(_password)));
-    }
-
- 
-    function getAllApplicants() public view returns (Applicant[] memory) {
-        return applicantList;
-    }
-
-   
-    function getAllSponsors() public view returns (Sponsor[] memory) {
-        return sponsorList;
+        emit SponsorRegistered(msg.sender, _name, _email);
     }
 
     
     function donate(address _applicant) public payable {
+        require(sponsors[msg.sender].exists, "Only sponsors can donate");
         require(applicants[_applicant].exists, "Applicant does not exist");
-        require(applicants[_applicant].verified, "Applicant is not verified");
         require(msg.value > 0, "Donation amount must be greater than 0");
 
         applicants[_applicant].fundsReceived += msg.value;
@@ -109,19 +74,47 @@ contract ScholarshipPlatform {
     }
 
    
-    function generateUniqueId(string memory _name, string memory _email) private pure returns (string memory) {
-        return string(abi.encodePacked(keccak256(abi.encodePacked(_name, _email))));
-    }
+    function withdraw(uint256 amount) public {
+        Applicant storage applicant = applicants[msg.sender];
+        require(applicant.exists, "Only applicants can withdraw");
+        require(applicant.fundsReceived >= amount, "Insufficient funds");
 
-  
-    function getApplicantDetails(address _applicant) public view returns (string memory, string memory, bool, uint256, string memory) {
-        Applicant memory applicant = applicants[_applicant];
-        return (applicant.name, applicant.personalEmail, applicant.verified, applicant.fundsReceived, applicant.proofIpfsHash);
+        applicant.fundsReceived -= amount;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Withdrawal failed");
+
+        emit Withdrawal(msg.sender, amount);
     }
 
    
+    function updateApplicantProfile(string memory _name, string memory _email, string memory _proofIpfsHash) public {
+        require(applicants[msg.sender].exists, "Only applicants can update profile");
+
+        applicants[msg.sender].name = _name;
+        applicants[msg.sender].personalEmail = _email;
+        applicants[msg.sender].proofIpfsHash = _proofIpfsHash;
+
+        emit ProfileUpdated(msg.sender, _name, _email);
+    }
+
+   
+    function updateSponsorProfile(string memory _name, string memory _email) public {
+        require(sponsors[msg.sender].exists, "Only sponsors can update profile");
+
+        sponsors[msg.sender].name = _name;
+        sponsors[msg.sender].email = _email;
+
+        emit ProfileUpdated(msg.sender, _name, _email);
+    }
+
+   
+    function getApplicantDetails(address _applicant) public view returns (string memory, string memory, uint256, string memory) {
+        Applicant memory applicant = applicants[_applicant];
+        return (applicant.name, applicant.personalEmail, applicant.fundsReceived, applicant.proofIpfsHash);
+    }
+
     function getSponsorDetails(address _sponsor) public view returns (string memory, string memory, bool) {
         Sponsor memory sponsor = sponsors[_sponsor];
-        return (sponsor.name, sponsor.uniqueId, sponsor.exists);
+        return (sponsor.name, sponsor.email, sponsor.exists);
     }
 }
